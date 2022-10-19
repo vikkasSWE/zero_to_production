088e14df9b19dfb2b0e9c246010c023e8303b3ad
Viktor Kasimir <kasimir.viktor@gmail.com>
2022-10-19 08:23:45 +0200

    cargo test task + testing framework for health check with random port

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
+use std::net::TcpListener;
 
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{dev::Server, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -10,14 +11,15 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
-pub async fn run() -> std::io::Result<()> {
-    HttpServer::new(|| {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
     })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    .listen(listener)?
+    .run();
+
+    Ok(server)
 }
